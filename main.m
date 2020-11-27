696071f39eec6a9119e5cdddd4e08429ddc5c9b2
Théo LE PAJOLEC <tlepajolec@enseirb-matmeca.fr>
2020-11-27 16:39:57 +0100

    J'ai fait du sale

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ clc;
 
 %% ZEBARTI
 
-img = double(imread('code_barre.png'));
+img = double(imread('../code_barre.png'));
 
 img_bw = (img(:,:,1) + img(:,:,2) + img(:,:,3))/3;
 
@@ -21,11 +21,24 @@ u = 1:U;
 
 
 
-M(:, 1) = X(1) + (u/(U-1))*(X(2) - X(1));
-M(:, 2) = Y(1) + (u/(U-1))*(Y(2) - Y(1));
+M(:, 1) = floor(X(1) + (u/(U-1))*(X(2) - X(1)));
+M(:, 2) = floor(Y(1) + (u/(U-1))*(Y(2) - Y(1)));
 
+for i=1:length(M)
+   profil(i)=img_bw(M(i,2),M(i,1)); 
 
-
-
+end
+code=[];
+for i=1:length(profil)
+    if profil(i)>125
+        code=[code 255*ones(500,1)];
+    else
+        code=[code ones(500,1)];
+    end
+end
+size(code)
+plot(profil);
+imshow(uint8(code));
+ylim([0 255]);
 
 
